14094fc0e9fa623808194911c3d398d11a7bd0a0
Bom Jang <dongwon1826@gmail.com>
2020-03-10 13:21:42 +0900

    반복문과 조건문을 이용해 구구단만들기/20.03.10

--- text.js
+++ text.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+var sum = 0;
+var n;
+var count = -1;
+while (n != 0) {
+    n = Number(prompt("숫자를 입력해 주세요(0 = 종료)"));
+    sum += n;
+    count++;
+    console.log(count, n, sum);
+}
